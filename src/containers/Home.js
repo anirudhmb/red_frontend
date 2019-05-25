505a93a301090c43c6c5932f7252a4f6e9d2a39e
abelathur <anirudh_belathur@intuit.com>
2019-05-25 19:12:28 +0530

    Login with Sessions.

---
+++ src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>ECP</h1>
+          <p>The Election Comission Portal Dashboard.</p>
+        </div>
+      </div>
+    );
+  }
+}
